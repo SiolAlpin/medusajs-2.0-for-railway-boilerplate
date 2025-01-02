a4c979ae4b20beff9bed3f0ee29aacf94a3ace83
Brandon <brandon@Brandons-MBP.vc.shawcable.net>
2025-01-02 17:19:23 -0300

    images test

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
-import "styles/styles.css"
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -7,13 +7,10 @@ const Hero = () => {
   <meta charSet="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Siol Alpin - All Info &amp; Shop</title>
-  <link rel="stylesheet" href="/styles.css" />
+  <link rel="stylesheet" href="../styles.css" />
   
 
-  <script defer src="/script.js"></script>
-  {/* Favicon */}
-  <link rel="icon" href="images/logo.png" type="image/x-icon" />
-  {/* Alternative format for PNG */}
+
   <link rel="icon" href="images/logo.png" type="image/png" />
   {/* Logo Section */}
   <div className="logo-section">
