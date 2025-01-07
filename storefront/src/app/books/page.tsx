4288776d2272d78b22550c3c08ac25299dd7d85b
Brandon <brandon@Brandons-MBP.vc.shawcable.net>
2025-01-07 02:29:44 -0300

    books

---
+++ storefront/src/app/books/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Books = () => {
+  return (
+    <div>
+      <h1>Welcome to My Page</h1>
+      <p>This is the content for the specific page.</p>
+    </div>
+  );
+};
+
+export default Books;
